90b518d70d762a104db57939ef27cc15385c8b35
Jerome Lecomte <jerome@herebefrogs.com>
2022-09-11 20:35:26 -0400

    add game submission description
    and a tweak to the zip report script to speadup tweeting!

--- scripts/zipSize.js
+++ scripts/zipSize.js
@@ -6,7 +6,7 @@ const limit = 1024 * 13;
 const remaining = limit - size;
 const percentage = Math.round((remaining / limit) * 100 * 100) / 100;
 console.log('\n-------------');
-console.log(`USED: ${size} BYTES`);
-console.log(`REMAINING: ${remaining} BYTES (${percentage}% of 13k budget)`);
+console.log(`Used: ${size} bytes`);
+console.log(`Remaining: ${remaining} bytes (${percentage}% of #js13k budget)`);
 console.log('-------------\n');
 
